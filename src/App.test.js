06a81322a9755a0e95570420d2c6cee3a8ba7b42
cathywalker <48182560+cathywalker@users.noreply.github.com>
2024-11-25 16:58:49 +0000

    add test to check if ChatComponent rendered

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ChatComponent using data-testid', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const chatComponent = screen.getByTestId('chat-component');
+  expect(chatComponent).toBeInTheDocument();
 });
